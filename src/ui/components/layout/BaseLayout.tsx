587b829f8fbcf05874b9541bc55942043772f8ea
ask-nugey <mischief.moi@gmail.com>
2024-01-22 00:34:37 +0900

    feat: css調整

--- src/ui/components/layout/BaseLayout.tsx
+++ src/ui/components/layout/BaseLayout.tsx
@@ -55,9 +55,6 @@ export const BaseLayout = ({ children }: PropsWithChildren) => {
     >
       <Header
         className={css({
-          display: "flex",
-          alignItems: "center",
-
           width: "100%",
           boxShadow:
             "0 1px 2px 0 rgba(0, 0, 0, 0.03), 0 1px 6px -1px rgba(0, 0, 0, 0.02), 0 2px 4px 0 rgba(0, 0, 0, 0.02)",
@@ -67,65 +64,76 @@ export const BaseLayout = ({ children }: PropsWithChildren) => {
           },
         })}
       >
-        <Link
-          href="/"
-          className={css({
-            display: "grid",
-            gridTemplateColumns: "auto 1fr",
-            gap: 2,
-            alignItems: "center",
-
-            color: "primary.600",
-            fontSize: "xl",
-            fontWeight: "bold",
-          })}
-        >
-          <Image alt="ヌギー(nugey)" src="/logo.png" width={56} height={56} />
-          Ask Nugey
-        </Link>
-        <Menu
-          mode="horizontal"
-          onClick={(e) => {
-            setCurrent(e.key);
-            router.push(e.key);
-          }}
-          selectedKeys={[current]}
-          items={MenuItems}
-          className={css({
-            flex: 1,
-            justifyContent: "flex-end",
-            minWidth: 0,
-            borderWidth: 0,
-            "& li": {
-              display: "flex",
-              justifySelf: "center",
-              alignItems: "center",
-              color: "primary.500",
-              fontWeight: "bold",
-            },
-          })}
-        />
         <div
           className={css({
             display: "flex",
+            alignItems: "center",
+
+            maxWidth: 1024,
+            height: "inherit",
+            marginInline: "auto",
           })}
         >
-          {SnsItems.map((item) => (
-            <Link
-              key={item.key}
-              href={item.key}
-              target="_blank"
-              className={css({
-                padding: 2,
-                transition: "all 0.1s ease",
-                _hover: {
-                  transform: "scale(1.2)",
-                },
-              })}
-            >
-              {item.icon}
-            </Link>
-          ))}
+          <Link
+            href="/"
+            className={css({
+              display: "grid",
+              gridTemplateColumns: "auto 1fr",
+              gap: 2,
+              alignItems: "center",
+
+              color: "primary.600",
+              fontSize: "xl",
+              fontWeight: "bold",
+            })}
+          >
+            <Image alt="ヌギー(nugey)" src="/logo.png" width={56} height={56} />
+            Ask Nugey
+          </Link>
+          <Menu
+            mode="horizontal"
+            onClick={(e) => {
+              setCurrent(e.key);
+              router.push(e.key);
+            }}
+            selectedKeys={[current]}
+            items={MenuItems}
+            className={css({
+              flex: 1,
+              justifyContent: "flex-end",
+              minWidth: 0,
+              borderWidth: 0,
+              "& li": {
+                display: "flex",
+                justifySelf: "center",
+                alignItems: "center",
+                color: "primary.500",
+                fontWeight: "bold",
+              },
+            })}
+          />
+          <div
+            className={css({
+              display: "flex",
+            })}
+          >
+            {SnsItems.map((item) => (
+              <Link
+                key={item.key}
+                href={item.key}
+                target="_blank"
+                className={css({
+                  padding: 2,
+                  transition: "all 0.1s ease",
+                  _hover: {
+                    transform: "scale(1.2)",
+                  },
+                })}
+              >
+                {item.icon}
+              </Link>
+            ))}
+          </div>
         </div>
       </Header>
       <Content>{children}</Content>
