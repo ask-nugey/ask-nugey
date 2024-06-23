40eddfe7f3a69c529973bd1afe33b29d2a8a0a2b
ask-nugey <yuki.iwase@blueish.co.jp>
2024-06-24 04:46:54 +0900

    💬: 記事のhxを全て1つ増やした

--- src/post/sample1/meta.ts
+++
@@ -1,9 +0,0 @@
-import { PostMeta } from "@/src/app/posts/_type";
-
-export const meta: PostMeta = {
-  title: "タイトル1",
-  description: "ディスクリプション1",
-  createdAt: new Date("2024-06-23"),
-  updatedAt: new Date("2024-06-23"),
-  tags: ["Rust"],
-};
