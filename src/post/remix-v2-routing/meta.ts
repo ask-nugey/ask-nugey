51085a3a58a7486dfbafae9aeba62110c09bffe5
ask-nugey <yuki.iwase@blueish.co.jp>
2025-03-03 04:50:33 +0900

    📝 docs: remix-v2-routing

---
+++ src/post/remix-v2-routing/meta.ts
@@ -0,0 +1,9 @@
+import { PostMeta } from '@/src/types/post';
+
+export const meta: PostMeta = {
+	title: 'Remix（v2）のルーティング',
+	description: 'Remix（v2）のルーティング（routing）についてv1と比較しつつ解説',
+	createdAt: new Date('2025-03-03'),
+	// updatedAt: new Date("2024-01-01"),
+	tags: ['Remix'],
+};
