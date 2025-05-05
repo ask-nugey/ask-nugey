0fe7ff7fb277ed8f65eabd8017a93ae6e8229f66
ask-nugey <yuki.iwase@blueish.co.jp>
2025-05-05 16:14:18 +0900

    ♻️ refactor: グローバルCSSにOne Darkテーマ、KaTeXのスタイルを追加しheadのリンクを削除

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,18 +46,6 @@ const providers = [AntdRegistry, ThemeProvider, BaseLayout];
 export default function RootLayout({ children }: PropsWithChildren) {
 	return (
 		<html lang="ja">
-			<head>
-				<link
-					rel="stylesheet"
-					href="https://cdn.jsdelivr.net/npm/katex@0.15.3/dist/katex.min.css"
-					integrity="sha384-KiWOvVjnN8qwAZbuQyWDIbfCLFhLXNETzBQjA/92pIowpC0d2O3nppDGQVgwd2nB"
-					crossOrigin="anonymous"
-				/>
-				<link
-					href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.9.0/themes/prism-tomorrow.min.css"
-					rel="stylesheet"
-				/>
-			</head>
 			<body className={inter.className}>
 				<MergeProvider items={providers}>{children}</MergeProvider>
 			</body>
