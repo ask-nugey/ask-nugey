8a072b243be160f1ee6598248d5f042818b64617
ask-nugey <yuki.iwase@blueish.co.jp>
2025-08-01 03:29:24 +0900

    fix

--- apps/web/src/post/react19-useactionstate-hook/meta.ts
+++ apps/web/src/post/react19-useactionstate-hook/meta.ts
@@ -1,9 +1,9 @@
-import { PostMeta } from '@/src/types/post';
+import type { PostMeta } from "@/src/models/post";
 
 export const meta: PostMeta = {
-	title: 'React19の新しいhooks「useActionState」',
+	title: "React19の新しいhooks「useActionState」",
 	description:
-		'React 19で導入された新しいフックuseActionStateの使い方と特徴を解説します',
-	createdAt: new Date('2025-07-28'),
-	tags: ['React', 'TypeScript'],
+		"React 19で導入された新しいフックuseActionStateの使い方と特徴を解説します",
+	createdAt: new Date("2025-07-28"),
+	tags: ["React", "TypeScript"],
 };
